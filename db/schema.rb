559a01c183bcfacd9f5471aef87dab68ae4ce164
shema@19974 <musonerwaprince@gmail.com>
2019-09-23 13:24:35 +0000

    After creation of branch

--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,15 @@
-ActiveRecord::Schema.define(version: 2019_09_23_091545) do
+ActiveRecord::Schema.define(version: 2019_09_23_120631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
