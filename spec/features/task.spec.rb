74163e7268e6fa76b2ca5f17882292fa3fadcbcd
shema@19974 <musonerwaprince@gmail.com>
2019-10-02 13:45:27 +0000

    After reverting to implentation of search by title and status

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -9,13 +9,9 @@ RSpec.feature "Task management function", type: :feature do
     FactoryBot.create(:task)
     FactoryBot.create(:second_task)
   end
+
   scenario "Test task list" do    
-    
-  # visit to tasks_path (transition to task list page)
   visit tasks_path
-
-  # write a test to verify that the string "" testtesttest "" samplesample "is included when accessing the task list page using have_content method
-
   expect(page).to have_content 'Student'
   expect(page).to have_content 'Accountant'
   end
@@ -34,20 +30,22 @@ RSpec.feature "Task management function", type: :feature do
     expect(page).to have_content 'A good student'    
   end
 
-  scenario "Test task details" do
-    # Task.create!(title: 'Employee', content: 'Is a good student')
-    visit tasks_path
-    click_link 'Show'
-    expect(page).to have_content 'A good student'
-  end
+  # scenario "Test task details" do
+  #   # Task.create!(title: 'Employee', content: 'Is a good student')
+  #   visit tasks_path
+  #   click_link 'Show'
+  #   expect(page).to have_content 'A good student'
+  # end
 
   scenario "Test whether tasks are arranged in descending order of creation date" do
-    
+    visit tasks_path
+    assert Task.order('created_at DESC')
   end
 
+  scenario "Test whether tasks are arranged in descending order of end_date" do
+    visit tasks_path
+    assert Task.order('end_date DESC')
+  end
 
-scenario "sort in  order " do
-assert Task.order('created ASC')
-end
 
 end
